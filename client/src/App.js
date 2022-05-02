a745c5e1fe01296037efc635807f450a1debf515
Gary Almes <garytalmes@gmail.com>
2022-05-02 09:34:38 -0400

    Added some router/page stuff.

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,13 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import Matchup from './pages/Matchup';
-import Vote from './pages/Vote';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
 
 function App() {
   return (
     <Router>
-      <div className="flex-column justify-center align-center min-100-vh bg-primary">
-        <Routes>
-          <Route 
-            path="/" 
-            element={<Home />} 
-          />
-          <Route 
-            path="/matchup" 
-            element={<Matchup />} 
-          />
-          <Route 
-            path="/matchup/:id" 
-            element={<Vote />} 
-          />
-        </Routes>
-      </div>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
     </Router>
   );
 }
